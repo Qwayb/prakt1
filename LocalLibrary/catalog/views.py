5d184d90e71de0ed1ced8f45f9daf03376875c19
Qwayb <65600587+Qwayb@users.noreply.github.com>
2024-10-25 23:29:43 +0700

    ListAndDetail

--- LocalLibrary/catalog/views.py
+++ LocalLibrary/catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """
@@ -20,3 +21,20 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    """Generic class-based list view for a list of authors."""
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    """Generic class-based detail view for an author."""
+    model = Author
