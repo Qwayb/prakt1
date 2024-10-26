07af6617781a9c795ded867e922adc0eabffbf19
Qwayb <65600587+Qwayb@users.noreply.github.com>
2024-10-26 21:45:28 +0700

    userSessions

--- LocalLibrary/catalog/admin.py
+++ LocalLibrary/catalog/admin.py
@@ -24,9 +24,9 @@ class BookAdmin(admin.ModelAdmin):
     inlines = [BooksInstanceInline]
 
 
-# Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
@@ -34,6 +34,6 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('book','imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back','borrower')
         }),
     )
--- LocalLibrary/catalog/models.py
+++ LocalLibrary/catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from datetime import date
 
 class MyModelName(models.Model):
     """Типичный класс модели, производный от класса Model."""
@@ -91,6 +93,7 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ["due_back"]
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
 
     def __str__(self):
@@ -98,6 +101,15 @@ class BookInstance(models.Model):
         String for representing the Model object
         """
         return '%s (%s)' % (self.id,self.book.title)
+
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
 
 class Author(models.Model):
     """
